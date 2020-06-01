3a8af9d34001d63dae632c46860663e51434a9f2
Yago Ananias <yago_souza92@yahoo.com.br>
2020-06-01 02:50:42 -0300

    Iniciando Aplicação

--- App.js
+++ App.js
@@ -1,36 +1,66 @@
-import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-
-const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
-});
-
-export default function App() {
+import React from 'react';
+import {SafeAreaView, ScrollView, StatusBar} from 'react-native';
+ 
+import SegmentedRoundDisplay from 'react-native-segmented-round-display';
+ 
+const App = () => {
+  const examples = [
+    {
+      displayValue: true,
+      formatValue: (value) => `R$ ${value.toFixed(2)}`,
+      segments: [
+        {
+          total: 80,
+          filled: 40,
+        },
+      ],
+    },
+    {
+      displayValue: true,
+      formatValue: (value) => `R$ ${value.toFixed(2)}`,
+      segments: [
+        {
+          total: 80,
+          filled: 80,
+        },
+        {
+          total: 30,
+          filled: 15,
+        },
+      ],
+    },
+    {
+      displayValue: true,
+      formatValue: (value) => `R$ ${value.toFixed(2)}`,
+      segments: [
+        {
+          total: 80,
+          filled: 80,
+        },
+        {
+          total: 30,
+          filled: 30,
+        },
+        {
+          total: 100,
+          filled: 40,
+        },
+      ],
+    }
+  ];
+ 
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
-    </View>
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView contentInsetAdjustmentBehavior="automatic">
+          {examples.map((example, i) => (
+            <SegmentedRoundDisplay key={i} {...example} />
+          ))}
+        </ScrollView>
+      </SafeAreaView>
+    </>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+};
+ 
+export default App;
